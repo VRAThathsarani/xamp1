b6cdae0aa17f1a65c2c7bbb3d25349d686061e50
VRAThathsarani <148706621+VRAThathsarani@users.noreply.github.com>
2025-01-04 12:01:36 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -5,51 +5,55 @@ import java.awt.event.ActionListener;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args){
-
-        JFrame frame = new JFrame("Registration Form");
+        JFrame frame = new JFrame("Registration form");
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        frame.setSize(400,500);
+        frame.setSize(500,500);
+        frame.setResizable(false);
 
         JPanel panel = new JPanel();
         panel.setLayout(null);
         frame.add(panel);
 
-        JLabel signLabel = new JLabel("Sign Up");
-        signLabel.setBounds(150,10,200,30);
-        signLabel.setForeground(Color.BLUE);
-        frame.setResizable(false);
-        panel.add(signLabel);
+        JLabel signUp = new JLabel("Sign Up");
+        signUp.setBounds(225,20,100,25);
+        signUp.setFont(new Font("Arial",Font.BOLD,20));
+        signUp.setForeground(Color.BLUE);
+        panel.add(signUp);
 
-        JLabel nameLabel = new JLabel("Name");
-        nameLabel.setBounds(50,60,100,25);
+        JLabel nameLabel = new JLabel("Name :");
+        nameLabel.setBounds(20,70,100,25);
+        nameLabel.setFont(new Font("Arial",Font.BOLD,20));
         panel.add(nameLabel);
 
-        JTextField nameText = new JTextField();
-        nameText.setBounds(150,60,100,25);
+        JTextField nameText  = new JTextField();
+        nameText.setBounds(200,70,200,25);
         panel.add(nameText);
 
-        JLabel emailLabel = new JLabel("Email");
-        emailLabel.setBounds(50,120,100,25);
+        JLabel emailLabel = new JLabel("Email :");
+        emailLabel.setBounds(20,120,100,25);
+        emailLabel.setFont(new Font("Arial",Font.BOLD,20));
         panel.add(emailLabel);
 
         JTextField emailText = new JTextField();
-        emailText.setBounds(150,120,100,25);
+        emailText.setBounds(200,120,200,25);
         panel.add(emailText);
 
-        JLabel genderLabel = new JLabel("Gender");
-        genderLabel.setBounds(50,180,100,25);
+        JLabel genderLabel = new JLabel("Gender :");
+        genderLabel.setBounds(20,170,100,25);
+        genderLabel.setFont(new Font("Arial",Font.BOLD,20));
         panel.add(genderLabel);
 
         JRadioButton maleButton = new JRadioButton("Male");
-        maleButton.setBounds(150,180,100,25);
+        maleButton.setBounds(200,170,100,25);
+        maleButton.setFont(new Font("Arial",Font.BOLD,20));
         panel.add(maleButton);
 
         JRadioButton femaleButton = new JRadioButton("Female");
-        femaleButton.setBounds(250,180,100,25);
+        femaleButton.setBounds(300,170,100,25);
+        femaleButton.setFont(new Font("Arial",Font.BOLD,20));
         panel.add(femaleButton);
 
         ButtonGroup buttonGroup = new ButtonGroup();
@@ -58,47 +62,76 @@ public class Main {
 
         String gender = maleButton.isSelected() ? "Male" : "Female";
 
-        JLabel passwordLabel = new JLabel("Password");
-        passwordLabel.setBounds(50,240,100,25);
+        JLabel passwordLabel = new JLabel("Password :");
+        passwordLabel.setBounds(20,220,200,25);
+        passwordLabel.setFont(new Font("Arial",Font.BOLD,20));
         panel.add(passwordLabel);
 
         JTextField passwordText = new JTextField();
-        passwordText.setBounds(150,240,100,25);
+        passwordText.setBounds(200,220,200,25);
         panel.add(passwordText);
 
-        JCheckBox checkBox = new JCheckBox("I am not a robbot.");
-        checkBox.setBounds(50,300,300,25);
+        JCheckBox checkBox = new JCheckBox("I am not a robot.");
+        checkBox.setBounds(20,270,300,25);
         panel.add(checkBox);
 
-        JButton submitButton = new JButton("Sign Up");
-        submitButton.setBackground(Color.BLUE);
-        submitButton.setForeground(Color.WHITE);
-        submitButton.setBounds(50,360,300,25);
-        panel.add(submitButton);
+        String[] dates = {"1","2,","3","4","5"};
+        String[] months = {"Jan","Feb","March","April"};
+        String[] years = {"2001","2002","2003","2004"};
 
-        frame.setVisible(true);
+        JLabel dobLabel = new JLabel("DOB :");
+        dobLabel.setBounds(20,320,100,25);
+        dobLabel.setFont(new Font("Arial",Font.BOLD,20));
+        panel.add(dobLabel);
+
+        JComboBox<String> date = new JComboBox<>(dates);
+        date.setBounds(200,320,50,25);
+        panel.add(date);
+
+        JComboBox<String> month = new JComboBox<>(months);
+        month.setBounds(250,320,50,25);
+        panel.add(month);
+
+        JComboBox<String> year = new JComboBox<>(years);
+        year.setBounds(300,320,100,25);
+        panel.add(year);
+
+
+        JButton submitButton = new JButton("Sign Up");
+        submitButton.setBounds(200,370,100,25);
+        submitButton.setForeground(Color.WHITE);
+        submitButton.setBackground(Color.BLUE);
+        panel.add(submitButton);
 
         submitButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                String d = date.getSelectedItem() + (String)month.getSelectedItem() + year.getSelectedItem();
                 try {
-                    Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/user","root","");
+                    Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/new","root","");
 
-                    PreparedStatement pstm = connection.prepareStatement("INSERT INTO registration(name,email,gender,password) VALUES(?,?,?,?)");
+                    PreparedStatement ps = connection.prepareStatement("INSERT INTO user1(name,email,gender,password,DOB) VALUES(?,?,?,?,?)");
 
-                    pstm.setObject(1, nameText.getText());
-                    pstm.setObject(2, emailText.getText());
-                    pstm.setObject(3, gender);
-                    pstm.setObject(4, passwordText.getText());
+                    ps.setObject(1,nameText.getText());
+                    ps.setObject(2,emailText.getText());
+                    ps.setObject(3,gender);
+                    ps.setObject(4,passwordText.getText());
+                    ps.setObject(5,d);
+                    ps.executeUpdate();
 
-                    pstm.executeUpdate();
+                    System.out.println("Registration Successfully.");
 
-                    JOptionPane.showMessageDialog(null,"Sign Up successful", "Success",JOptionPane.INFORMATION_MESSAGE);
+                    JOptionPane.showMessageDialog(null,"Registration Successfully." ,"Sucess" ,JOptionPane.INFORMATION_MESSAGE);
 
-                } catch (SQLException ex) {
-                    throw new RuntimeException(ex);
+                } catch (Exception exception) {
+                    throw new RuntimeException(exception);
                 }
+
             }
         });
+
+
+
+        frame.setVisible(true);
     }
 }
